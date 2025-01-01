019693f41157d91f67f20171201a850e14e058d4
kouosi <kouosi@tuta.io>
2025-01-02 00:23:03 +0545

    feat: Optimize for mobile and SEO

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+/* Switch theme */
 function changeIcon() {
     if (document.body.dataset.theme == 'light') {
         themeSwitcher.innerHTML = "<i class='fa fa-sun'></i>";
@@ -17,4 +18,20 @@ themeSwitcher.addEventListener('click', () => {
     document.body.dataset.theme = newTheme;
     localStorage.setItem('theme', newTheme);
     changeIcon();
+});
+
+/* Toggle navbar */
+document.getElementById('dropdown-toggle').addEventListener('click', function () {
+    document.querySelector('nav').classList.toggle('open');
+});
+
+/* Disable navbar on click */
+document.addEventListener('click', function (event) {
+    const nav = document.querySelector('nav');
+    const toggle = document.getElementById('dropdown-toggle');
+
+    // Check if the click is outside the navigation menu and the toggle button
+    if (!nav.contains(event.target) && !toggle.contains(event.target)) {
+        nav.classList.remove('open');
+    }
 });
